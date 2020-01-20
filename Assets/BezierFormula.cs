a61fb040968cf5e63e111678b416a4fa809414ca
joseph <auctux@gmail.com>
2020-01-20 10:41:38 +0530

    initial commit

---
+++ Assets/BezierFormula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFormula : MonoBehaviour
+{
+    [SerializeField] Transform[] targetPositions;
+    Vector2 pos;
+    private void OnDrawGizmos()
+    {
+        for (float t = 0; t <= 1; t += 0.05f)
+        {
+            Gizmos.color = Color.white;
+
+            pos = Mathf.Pow(1 - t, 3) * targetPositions[0].position + 3 *
+                Mathf.Pow(1 - t, 2) * t * targetPositions[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * targetPositions[2].position +
+                Mathf.Pow(t, 3) * targetPositions[3].position;
+            Gizmos.DrawSphere(pos, 0.2f);
+            
+        }
+
+        Gizmos.DrawLine(new Vector2(targetPositions[0].position.x, targetPositions[0].position.y),
+                new Vector2(targetPositions[1].position.x, targetPositions[1].position.y)
+            );
+        Gizmos.DrawLine(new Vector2(targetPositions[2].position.x, targetPositions[2].position.y),
+            new Vector2(targetPositions[3].position.x, targetPositions[3].position.y));
+    }
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] private Transform[] path;
+    private int pathIndex;
+    private bool freeToGo;
+    private float parameter;
+   
+    private float speed ;
+    private Vector2 player_position;
+
+    private void Start()
+    {
+        freeToGo = true;
+        pathIndex = 0;
+        parameter = 0;
+        speed = 0.5f;
+    }
+    private void Update()
+    {
+        if (freeToGo)
+            StartCoroutine(bezierCurves(pathIndex));
+
+    }
+    IEnumerator bezierCurves(int _index)
+    {
+        freeToGo = false; 
+
+       Vector2 P0 = path[_index].GetChild(0).position;
+       Vector2 P1 = path[_index].GetChild(1).position;
+       Vector2 P2 = path[_index].GetChild(2).position;
+       Vector2 P3 = path[_index].GetChild(3).position;
+
+        while (parameter < 1) 
+        {
+            parameter += speed * Time.deltaTime;
+
+            player_position = Mathf.Pow(1 - parameter, 3) * P0 +
+                3 * Mathf.Pow(1 - parameter, 2) * parameter * P1 +
+                3 * (1 - parameter) * Mathf.Pow(parameter, 2) * P2 +
+                Mathf.Pow(parameter, 3) * P3;
+
+            transform.position = player_position;
+            yield return new WaitForEndOfFrame();
+        }
+        parameter = 0;
+        pathIndex += 1;
+
+        if (pathIndex > path.Length - 1) 
+        {
+            pathIndex = 0;
+        }
+        freeToGo = true;
+    }
+}
